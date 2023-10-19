92f5d5e41e5e64ba0c0a4d39f96467ff9a7f019a
Elliot <ellthrowaway2@icloud.com>
2023-10-19 16:11:44 -0700

    starting styling with bootstrap

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <>
             <div className="home-landing">
-                {/* <img src="src/assets/home-banner.jpg" alt="A picture of a laptop, a phone, a notebook, and a mug on a desk. A small potted cactus is in the background. The words 'community', 'accessibility', and 'equity' are superimposed." className="home-img" /> */}
+                <img src="src/assets/home-banner.jpg" alt="A picture of a laptop, a phone, a notebook, and a mug on a desk. A small potted cactus is in the background. The words 'community', 'accessibility', and 'equity' are superimposed." className="home-img" />
             </div>
         </>
     )
