4d120f30f32c7ac9ee8300996eb21515427ad932
Elliot <ellthrowaway2@icloud.com>
2023-10-19 16:25:12 -0700

    created collapsible navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,43 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom"
 export default function Nav() {
     // content
     return (
-        <div className="nav">
-            <Link to='/' className='nav-link main-brand'>Home</Link>
-            <Link to='/about' className='nav-link main-brand'>About</Link>
-            <Link to='/projects' className='nav-link main-brand'>Projects</Link>
-            <Link to='/contact' className='nav-link main-brand'>Contact</Link>
+        <div className='nav navbar navbar-expand-lg'>
+            <button
+                className='navbar-toggler'
+                type='button'
+                data-bs-toggle='collapse'
+                data-bs-target='#navbarNav'
+                aria-controls='navbarNav'
+                aria-expanded='false'
+                aria-label='Toggle navigation'
+            >
+                <span className='navbar-toggler-icon'></span>
+            </button>
+            <div className='collapse navbar-collapse' id='navbarNav'>
+                <ul className='navbar-nav'>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-link'>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/about' className='nav-link'>
+                            About
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/projects' className='nav-link'>
+                            Projects
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/contact' className='nav-link'>
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
