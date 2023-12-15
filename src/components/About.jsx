795cce8c54a7fd8ad40f589d1ab418f67d6840e8
Elliot <ellthrowaway2@icloud.com>
2023-12-15 13:31:49 -0800

    adding about page image, minor style changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,26 @@
 export default function About() {
     return (
-        <div className="about">
-            <h2 className="about-head main-brand">About</h2>
-            <div className="about-main">
-                    <p>Elliot Hill is a full stack software developer relocating to San Francisco, CA in January 2024 to pursue his engineering career goals (and to be closer to his new niece!)</p>
-                    <p>With a broad background in customer service, political science, and diversity and inclusion advocacy, he approaches projects through a lens of accessibility, equity, and integrity.</p>
+        <div className='about'>
+            <h2 className='about-head main-brand'>About</h2>
+            <div className='container w-50'>
+                <p>
+                    Elliot Hill is a full stack software developer relocating to
+                    San Francisco, CA in January 2024 to pursue his engineering
+                    career goals (and to be closer to his new niece!)
+                </p>
+                <p>
+                    With a broad background in customer service, political
+                    science, and diversity and inclusion advocacy, he approaches
+                    projects through a lens of accessibility, equity, and
+                    integrity.
+                </p>
             </div>
-            <div className="img-profile">
-                {/* <img src="#" alt="A headshot of Elliot wearing a blue suit and tie with greenery in the background."/> */}
+            <div className='container'>
+                <img
+                    src='src/assets/LI profile pic.jpeg'
+                    alt='A headshot of Elliot wearing a blue suit and tie with greenery in the background.'
+                    className="img-thumbnail rounded-circle w-25"
+                />
             </div>
         </div>
     )
