c69b3f485c6177d55c15f69b4bb2e36b057e17bd
Elliot <ellthrowaway2@icloud.com>
2023-12-19 16:44:50 -0800

    about page layout adjustments

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,22 +2,25 @@ export default function About() {
     return (
         <div className='container'>
             <h2 className='about-head main-brand'>About</h2>
-            <div className='container w-50 d-flex flex-column'>
-                <p>
-                    Elliot Hill is a full stack software developer relocating to
-                    San Francisco, CA in January 2024 to pursue his engineering
-                    career goals (and to be closer to his new niece!)
-                </p>
-                <p>
-                    With a broad background in customer service, political
-                    science, and diversity and inclusion advocacy, he approaches
-                    projects through a lens of accessibility, equity, and
-                    integrity.
-                </p>
+            <div className='container d-flex flex-row'>
+                <div className='container w-50 d-flex flex-column my-auto'>
+                    <p>
+                        Elliot Hill is a full stack software developer
+                        relocating to San Francisco, CA in January 2024 to
+                        pursue his engineering career goals (and to be closer to
+                        his new niece!).
+                    </p>
+                    <p>
+                        With a broad background in customer service, political
+                        science, and diversity and inclusion advocacy, he
+                        approaches projects through a lens of accessibility,
+                        equity, and integrity.
+                    </p>
+                </div>
                 <img
                     src='src/assets/LI-profile-pic-sq.jpeg'
                     alt='A headshot of Elliot wearing a blue suit and tie with greenery in the background.'
-                    className="img-thumbnail rounded-circle w-25"
+                    className='img-thumbnail rounded-circle w-25 mx-auto'
                 />
             </div>
         </div>
